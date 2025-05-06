128c80d750faece60a839e96c265a65c969415c7
onward0101 <zenochiang0101@@gmail.com>
2025-05-06 17:23:45 +0800

    Add: warmup and karel'home in challlenges

---
+++ Challenges/02_Karel's Home/main.py
@@ -0,0 +1,34 @@
+from karel.stanfordkarel import *
+
+# File: shelter.py
+# -----------------------------
+# The warmup program defines a "main"
+# function which should make Karel 
+# move to the beeper, pick it up, and
+# return home.
+def main():
+    move()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    turn_left()
+    turn_left()
+    while front_is_clear():
+        move()
+    turn_right()
+    move()
+    turn_right()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()   
+    
+    
+# don't edit these next two lines
+# they tell python to run your main function
+if __name__ == '__main__':
+    main()
